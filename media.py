68dc09ac2d1e8045b1d076c54fe66883d233f6e3
Christopher Aubuchon <aubuchcl@gmail.com>
2017-06-24 00:47:34 -0700

    changed python pages to reflect formatting of pep 8 and added comments

--- media.py
+++ media.py
@@ -1,6 +1,7 @@
 import webbrowser
 
 class Movie():
+	# initialize the class movie
 	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
 
 		self.title = movie_title
@@ -8,6 +9,7 @@ class Movie():
 		self.poster_image_url = poster_image
 		self.trailer_youtube_url = trailer_youtube
 
+	# this method allows you to access the movie objects trailer
 	def show_trailer(self):
 		webbrowser.open(self.trailer_youtube_url)
 
